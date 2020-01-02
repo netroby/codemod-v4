3bf34b297e9839efb8e8734ffa2ae9942a2b17f8
vagusX <vagusxl@gmail.com>
2020-01-03 00:02:52 +0800

    feat: update

--- transforms/v3-Modal-method-with-icon-to-v4.js
+++ transforms/v3-Modal-method-with-icon-to-v4.js
@@ -31,7 +31,7 @@ module.exports = (file, api, options) => {
         const localComponentName = path.parent.node.local.name;
         const antdPkgName = path.parent.parent.node.source.value;
 
-        root
+        const paths = root
           .find(j.CallExpression, {
             callee: {
               object: {
@@ -45,28 +45,32 @@ module.exports = (file, api, options) => {
           })
           .filter(nodePath =>
             modalMethodNames.includes(nodePath.node.callee.property.name),
-          )
-          .forEach(nodePath => {
-            if (
-              !Array.isArray(nodePath.node.arguments) ||
-              !nodePath.node.arguments[0] ||
-              nodePath.node.arguments[0].type !== 'ObjectExpression'
-            ) {
-              return;
-            }
+          );
 
-            const args = nodePath.node.arguments[0];
-            const iconProperty = args.properties.find(
-              property =>
-                property.key.type === 'Identifier' &&
-                property.key.name === 'icon' &&
-                property.value.type === 'StringLiteral',
-            );
+        paths.forEach(nodePath => {
+          if (
+            !Array.isArray(nodePath.node.arguments) ||
+            !nodePath.node.arguments[0] ||
+            nodePath.node.arguments[0].type !== 'ObjectExpression'
+          ) {
+            return;
+          }
 
-            if (!iconProperty) {
-              return;
-            }
+          const args = nodePath.node.arguments[0];
+          const iconProperty = args.properties.find(
+            property =>
+              property.key.type === 'Identifier' &&
+              property.key.name === 'icon',
+          );
 
+          // v3-Icon-to-v4-Icon should handle with JSXElement
+          if (!iconProperty || iconProperty.value.type === 'JSXElement') {
+            return;
+          }
+
+          hasChanged = true;
+
+          if (iconProperty.value.type === 'StringLiteral') {
             const v3IconName = iconProperty.value.value;
             const v4IconComponentName = getV4IconComponentName(v3IconName);
             if (v4IconComponentName) {
@@ -81,9 +85,37 @@ module.exports = (file, api, options) => {
                 importedName: v4IconComponentName,
                 before: antdPkgName,
               });
-              hasChanged = true;
+              return;
+            } else {
+              const location = nodePath.node.loc.start;
+              const message =
+                'Contains an invalid icon, please check it at https://ant.design/components/icon';
+              summary.appendLine(
+                `${file.path} - ${location.line}:${location.column}`,
+                j(nodePath).toSource(),
+                message,
+              );
             }
+          }
+
+          // handle it with `@ant-design/compatible`
+          const typeAttr = j.jsxAttribute(
+            j.jsxIdentifier('type'),
+            j.jsxExpressionContainer(iconProperty.value),
+          );
+          const iconJSXElement = createIconJSXElement(j, 'LegacyIcon', [
+            typeAttr,
+          ]);
+          iconProperty.value = iconJSXElement;
+
+          // add @ant-design/compatible imports
+          addSubmoduleImport(j, root, {
+            moduleName: '@ant-design/compatible',
+            importedName: 'Icon',
+            localName: 'LegacyIcon',
+            before: antdPkgName,
           });
+        });
       });
 
     return hasChanged;
